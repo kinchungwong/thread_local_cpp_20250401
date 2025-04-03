60e7f0d69a743a51150e86fb560d22cae019965f
kinchungwong <kinchungwong@users.noreply.github.com>
2025-04-02 19:01:52 -0700

    update build.sh
    Use build subdir for all outputs

--- v0/build.sh
+++ v0/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
 
-g++ -std=c++11 -g -Og -DNDEBUG -o main.out main.cpp -I${PWD} -I${PWD}/include
-objdump -d -C -M intel ./main.out > main.s
+mkdir -p build
+g++ -std=c++11 -g -Og -DNDEBUG -o build/main.out main.cpp -I${PWD} -I${PWD}/include
+objdump -d -C -M intel ./build/main.out > ./build/main.s
